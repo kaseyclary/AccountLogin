695cca99b6bdc00ff610e904cb6c1d02577c5021
Kasey <mail.kaseyc@gmail.com>
2021-11-10 22:34:54 -0600

    finished db and backend

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://kaseycdev:cobaltSS!53@cluster0.n801g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" 
+    mongoURI: "mongodb+srv://kaseycdev:cobaltSS!53@cluster0.n801g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" ,
+    secretOrKey: "secret"
   };
